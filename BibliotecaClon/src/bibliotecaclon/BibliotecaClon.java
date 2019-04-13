ad63098b0f1481dc4a3c5cd251a957016b904cec
Ivan <Ivan@LAPTOP-1I25KK98>
2019-04-13 10:47:40 -0300

    Esto es un clon

---
+++ BibliotecaClon/src/bibliotecaclon/BibliotecaClon.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibliotecaclon;
+
+/**
+ *
+ * @author Ivan
+ */
+public class BibliotecaClon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    int numero = 10;
+    
+}
