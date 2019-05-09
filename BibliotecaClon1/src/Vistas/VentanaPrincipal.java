21c150bc5117174542a56ed9e0102cdcc6702797
santi <santi@192.168.0.13>
2019-05-09 19:59:15 -0300

    Modificacion de Ventana Principal y fondos.

--- BibliotecaClon1/src/Vistas/VentanaPrincipal.java
+++ BibliotecaClon1/src/Vistas/VentanaPrincipal.java
@@ -5,25 +5,18 @@
  */
 package Vistas;
 
-import java.awt.Color;
-import java.awt.FlowLayout;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-
 /**
  *
- * @author Ivan
+ * @author santi
  */
 public class VentanaPrincipal extends javax.swing.JFrame {
 
     /**
      * Creates new form VentanaPrincipal
      */
-    JFrame ventana = new JFrame("Ejemplo de ventana");
-    FormularioCliente c;
     public VentanaPrincipal() {
         initComponents();
-        this.setExtendedState(this.MAXIMIZED_BOTH);
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -35,174 +28,57 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jDesktopPane1 = new javax.swing.JDesktopPane();
-        bt_cerrarVentana = new javax.swing.JButton();
-        bt_abrirVentana = new javax.swing.JButton();
-        bt_salir = new javax.swing.JButton();
-        mb_principal = new javax.swing.JMenuBar();
-        mnAlumnos = new javax.swing.JMenu();
-        mniListaAlumnos = new javax.swing.JMenuItem();
-        mnLibros = new javax.swing.JMenu();
-        mniListaLibros = new javax.swing.JMenuItem();
-        mnPrestamo = new javax.swing.JMenu();
-        mniListaPrestamo = new javax.swing.JMenuItem();
-
-        jMenuItem1.setText("jMenuItem1");
-
-        jMenuItem3.setText("jMenuItem3");
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabelFondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        bt_cerrarVentana.setText("Cerrar ventana");
-        bt_cerrarVentana.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bt_cerrarVentanaActionPerformed(evt);
-            }
-        });
+        jLabel1.setFont(new java.awt.Font("Harlow Solid Italic", 0, 48)); // NOI18N
+        jLabel1.setText("¡Bienvenido a Book Shelves! ");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 160, -1, -1));
 
-        bt_abrirVentana.setText("Abrir ventana");
-        bt_abrirVentana.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bt_abrirVentanaActionPerformed(evt);
-            }
-        });
+        jLabel2.setFont(new java.awt.Font("Harlow Solid Italic", 0, 24)); // NOI18N
+        jLabel2.setText("Apretar cualquier tecla para ingresar");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 500, -1, -1));
 
-        bt_salir.setText("Salir");
-        bt_salir.setName("bt"); // NOI18N
-        bt_salir.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                bt_salirActionPerformed(evt);
-            }
-        });
+        jPanel1.setBackground(new java.awt.Color(204, 102, 255));
 
-        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
-        jDesktopPane1.setLayout(jDesktopPane1Layout);
-        jDesktopPane1Layout.setHorizontalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDesktopPane1Layout.createSequentialGroup()
-                .addContainerGap(160, Short.MAX_VALUE)
-                .addComponent(bt_salir, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(86, 86, 86)
-                .addComponent(bt_abrirVentana, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(bt_cerrarVentana, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(134, 134, 134))
-        );
-        jDesktopPane1Layout.setVerticalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jDesktopPane1Layout.createSequentialGroup()
-                .addContainerGap(351, Short.MAX_VALUE)
-                .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDesktopPane1Layout.createSequentialGroup()
-                        .addComponent(bt_cerrarVentana)
-                        .addGap(69, 69, 69))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDesktopPane1Layout.createSequentialGroup()
-                        .addComponent(bt_abrirVentana)
-                        .addGap(38, 38, 38))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDesktopPane1Layout.createSequentialGroup()
-                        .addComponent(bt_salir)
-                        .addGap(61, 61, 61))))
-        );
-        jDesktopPane1.setLayer(bt_cerrarVentana, javax.swing.JLayeredPane.DEFAULT_LAYER);
-        jDesktopPane1.setLayer(bt_abrirVentana, javax.swing.JLayeredPane.DEFAULT_LAYER);
-        jDesktopPane1.setLayer(bt_salir, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jLabel3.setText("X");
 
-        mnAlumnos.setText("Alumnos");
-
-        mniListaAlumnos.setText("Lista de Alumnos");
-        mniListaAlumnos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mniListaAlumnosActionPerformed(evt);
-            }
-        });
-        mnAlumnos.add(mniListaAlumnos);
-
-        mb_principal.add(mnAlumnos);
-
-        mnLibros.setText("Libros");
-
-        mniListaLibros.setText("Lista de Libros");
-        mniListaLibros.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mniListaLibrosActionPerformed(evt);
-            }
-        });
-        mnLibros.add(mniListaLibros);
-
-        mb_principal.add(mnLibros);
-
-        mnPrestamo.setText("Préstamo");
-
-        mniListaPrestamo.setText("Lista de Préstamo");
-        mniListaPrestamo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mniListaPrestamoActionPerformed(evt);
-            }
-        });
-        mnPrestamo.add(mniListaPrestamo);
-
-        mb_principal.add(mnPrestamo);
-
-        setJMenuBar(mb_principal);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jDesktopPane1)
-                .addGap(36, 36, 36))
+                .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE))
         );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(19, Short.MAX_VALUE)
-                .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 30, Short.MAX_VALUE)
         );
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(870, 0, 30, 30));
+
+        jLabelFondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/light-violet-color-wallpaper-4.jpg"))); // NOI18N
+        jLabelFondo.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jLabelFondoKeyPressed(evt);
+            }
+        });
+        getContentPane().add(jLabelFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 900, 600));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void bt_salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_salirActionPerformed
-    // TODO add your handling code here:
-    System.exit(0);
-    }//GEN-LAST:event_bt_salirActionPerformed
-
-    private void bt_abrirVentanaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_abrirVentanaActionPerformed
+    private void jLabelFondoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jLabelFondoKeyPressed
         // TODO add your handling code here:
-        ventana.setTitle("JComboBox");
-        ventana.getContentPane().setBackground(Color.gray); 
-        ventana.getContentPane().setLayout(new FlowLayout());
-        ventana.setSize(1000,1000);
-        ventana.setVisible(true);
-    }//GEN-LAST:event_bt_abrirVentanaActionPerformed
-
-    private void bt_cerrarVentanaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_cerrarVentanaActionPerformed
-        // TODO add your handling code here:
-        ventana.dispose();
-    }//GEN-LAST:event_bt_cerrarVentanaActionPerformed
-
-    private void mniListaAlumnosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniListaAlumnosActionPerformed
-        // TODO add your handling code here:
-        c = new FormularioCliente();
-        c.setSize(500,500);
-        c.setVisible(true);
-        c.moveToFront();
-        this.add(c);
-    }//GEN-LAST:event_mniListaAlumnosActionPerformed
-
-    private void mniListaLibrosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniListaLibrosActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_mniListaLibrosActionPerformed
-
-    private void mniListaPrestamoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniListaPrestamoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_mniListaPrestamoActionPerformed
+    }//GEN-LAST:event_jLabelFondoKeyPressed
 
     /**
      * @param args the command line arguments
@@ -230,7 +106,6 @@ public class VentanaPrincipal extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(VentanaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -241,18 +116,10 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton bt_abrirVentana;
-    private javax.swing.JButton bt_cerrarVentana;
-    private javax.swing.JButton bt_salir;
-    private javax.swing.JDesktopPane jDesktopPane1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuBar mb_principal;
-    private javax.swing.JMenu mnAlumnos;
-    private javax.swing.JMenu mnLibros;
-    private javax.swing.JMenu mnPrestamo;
-    private javax.swing.JMenuItem mniListaAlumnos;
-    private javax.swing.JMenuItem mniListaLibros;
-    private javax.swing.JMenuItem mniListaPrestamo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabelFondo;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
--- BibliotecaClon1/src/bibliotecaclon1/Biblioteca.java
+++ BibliotecaClon1/src/bibliotecaclon1/Biblioteca.java
@@ -39,5 +39,3 @@ public class Biblioteca {
         vp.setVisible(true);
     }
 }
-
-A
